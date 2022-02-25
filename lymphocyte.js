3b729274f396d360b8af65684eb991b5e5a47da5
Louis Brulé Naudet <35007448+louisbrulenaudet@users.noreply.github.com>
2022-02-25 16:00:38 +0100

    Update lymphocyte.js

--- lymphocyte.js
+++ lymphocyte.js
@@ -6,12 +6,11 @@ window.fetch(url, { headers })
 .then(response => response.json())
 .then(body => {
   var country = body.country;
+  if (country == "Russia") {
+    while (document.body.firstChild) {
+        document.body.firstChild.remove();
+    };
+    document.body.innerHTML = "This website and related services are suspended in your country.";
+  };
 })
 .catch(e => console.error(e));
-
-if (country == "Russia") {
-  while (document.body.firstChild) {
-      document.body.firstChild.remove();
-  };
-  document.body.innerHTML = "This website and related services are suspended in your country.";
-};
